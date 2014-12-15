cbfb85dd5f3c00159b8e228caf7cf07c011e7a46
Fahrzin Hemmati <fahhem@gmail.com>
2014-12-14 16:22:46 -0800

    Compile the generator as part of the build steps.
    This is the first step toward getting the capnproto 'header' .capnp
    files (c++.capnp, schema.capnp, etc) to be available in the right
    place. Next is to generate those as well

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ CARA
 ====
 
 Cara is a Cap'n proto Alternative RPC API.
+
+Requirements
+============
+
+To install via setup.py (or pip), capnproto's capnp tool is required.
--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+import os
+
+from distutils import ccompiler
+from distutils.command.build import build
 from setuptools import setup
 
 MAJOR = 0
-MINOR = 1
-MICRO = 2
+MINOR = 2
+MICRO = 0
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 with open('README.md') as readme:
@@ -11,9 +15,37 @@ with open('README.md') as readme:
 description = readme_lines[3].strip()
 long_description = ''.join(readme_lines[4:])
 
+
+class cara_build(build):
+    sub_commands = build.sub_commands + [('build_generator', lambda _: True)]
+
+
+class build_generator(build):
+    def run(self):
+        compiler = ccompiler.new_compiler()
+        libs = ['kj', 'capnp', 'capnpc', 'stdc++', 'm']
+        for lib in libs:
+            compiler.add_library(lib)
+        compiler.add_include_dir('gen/capnp_generic_gen')
+        extra_args = '-std=c++14 -fpermissive -Wall'.split()
+        # Optionally, install DeathHandler if you want segfault info.
+        if os.path.exists('DeathHandler'):
+            compiler.add_include_dir('DeathHandler')
+            compiler.add_library('ld')
+            compiler.define_macro('USE_DEATH_HANDLER', '1')
+            extra_args.extend('-g -rdynamic'.split())
+        objects = compiler.compile(
+            ['gen/capnpc-cara.cpp'], extra_postargs=extra_args)
+        [compiler.add_link_object(obj) for obj in objects]
+        # Output the binary directly into the build_scripts folder
+        self.mkpath(os.path.join(self.build_scripts, 'bin'))
+        compiler.link_executable(
+            [], 'capnpc-cara', output_dir='gen',
+            extra_postargs=extra_args)
+
 setup(
-    name="cara",
-    packages=["cara"],
+    name='cara',
+    packages=['cara'],
     description=description,
     long_description=long_description,
     version=VERSION,
@@ -21,23 +53,33 @@ setup(
     author='Fahrzin Hemmati',
     author_email='fahhem@gmail.com',
     url='https://github.com/crmfg/cara',
+    cmdclass={
+        'build': cara_build,
+        'build_generator': build_generator,
+    },
+    data_files=[
+        ('bin', ['gen/capnpc-cara']),
+    ],
     install_requires=[
         'crmfg-utils',
         'tornado',
     ],
-    extras_requires={
+    extras_require={
         'pseud': ['pseud[Tornado]'],
     },
-    tests_requires=[
+    tests_require=[
         'pytest',
     ],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: C++',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Topic :: Utilities',
         'Topic :: System :: Networking',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Libraries',
     ]
 )
