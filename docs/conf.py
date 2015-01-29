382d7a54c0d67f7a657ec96702020d8b91ca03ad
Fahrzin Hemmati <fahhem@gmail.com>
2015-01-29 02:18:21 -0800

    docs/conf.py isn't necessary (and doesn't work)
    [ci skip]

--- docs/conf.py
+++
@@ -1,6 +0,0 @@
-import os
-
-generator_filename = os.path.join('gen', 'capnpc-cara')
-if not os.path.exists(generator_filename):
-    with open(generator_filename, 'w') as generator:
-        generator.write('')
