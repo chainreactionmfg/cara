daa0f7df3804dbb9653908abfa24e67d3cdf2a1f
Fahrzin Hemmati <fahhem@gmail.com>
2015-02-02 05:44:57 -0800

    Don't use zeromq in the tests, much less flaky

--- tests/cara_pseud_test.py
+++ tests/cara_pseud_test.py
@@ -1,6 +1,8 @@
-import tornado
+from unittest import mock
+
 import tornado.testing
 import pseud
+import zmq
 
 import cara
 from cara import cara_pseud
@@ -9,11 +11,51 @@ from tests.cara_pseud_test_capnp import FooIface, BarIface, BazIface
 
 class PseudTest(tornado.testing.AsyncTestCase):
 
-    def create_client_server(self):
+    def setUp(self):
+        super().setUp()
         self.endpoint = b'ipc://pseud-test-ipc'
+        # Don't let pseud make any real contexts.
+        patch = mock.patch.object(pseud.Server, '_make_context')
+        mock_context = patch.start()
+        self.addCleanup(patch.stop)
+        mock_context.return_value.socket.return_value.mechanism = zmq.PLAIN
+
+        # Return a stream mock that sends packets to the other stream.
+        patch = mock.patch.object(
+            zmq.eventloop.zmqstream, 'ZMQStream')
+        self.stream_mock = patch.start()
+        self.addCleanup(patch.stop)
+        self.stream_mock.side_effect = self.create_stream
+
+    def create_stream(self, socket, io_loop):
+        new_stream = mock.MagicMock()
+        new_stream.socket = socket
+        def send_effect(data, callback):
+            data = [zmq.Frame(frame) for frame in data]
+            # Send data to the _other_ stream.
+            other_stream = (
+                self.server_stream
+                if new_stream is self.client_stream
+                else self.client_stream)
+            call = other_stream.on_recv.mock_calls[0]
+            name, args, kwargs = call
+            with mock.patch.object(data[-1], 'get'):
+                # Have to make the User-Id return our username.
+                username = new_stream.socket.plain_username
+                if isinstance(username, (bytes, str)):
+                    data[-1].get.return_value = username.decode('utf-8')
+                args[0](data)
+            callback()
+        new_stream.send_multipart.side_effect = send_effect
+        return new_stream
+
+    def create_client_server(self):
         self.server = self.create_server()
         self.client = self.create_client()
-        return [self.server.start(), self.client.start()]
+        starts = [self.server.start(), self.client.start()]
+        self.server_stream = self.server.reader
+        self.client_stream = self.client.reader
+        return starts
 
     def create_server(self):
         server = pseud.Server(
@@ -24,11 +66,11 @@ class PseudTest(tornado.testing.AsyncTestCase):
     def create_client(self):
         client = pseud.Client(
             b'server', io_loop=self.io_loop,
-            security_plugin='plain', user_id=b'test', password=b'_')
+            security_plugin='plain', user_id=b'client', password=b'_')
         client.connect(self.endpoint)
         return cara_pseud.setup_client(client)
 
-    @tornado.testing.gen_test
+    @tornado.testing.gen_test(timeout=0.5)
     def test_simple(self):
         yield self.create_client_server()
 
@@ -37,9 +79,9 @@ class PseudTest(tornado.testing.AsyncTestCase):
             self.stop(True)
 
         yield self.client.test()
-        self.assertTrue(self.wait())
+        assert self.wait()
 
-    @tornado.testing.gen_test
+    @tornado.testing.gen_test(timeout=0.5)
     def test_call_client_cb(self):
         yield self.create_client_server()
 
@@ -54,7 +96,7 @@ class PseudTest(tornado.testing.AsyncTestCase):
         yield self.client.callback(Foo())
         assert self.wait()
 
-    @tornado.testing.gen_test
+    @tornado.testing.gen_test(timeout=0.5)
     def test_call_server_cb(self):
         yield self.create_client_server()
 
